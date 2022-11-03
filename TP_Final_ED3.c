3c02ad570fa63a437817c77c8a99126870caed61
Ema-Rodriguez <83190827+Ema-Rodriguez@users.noreply.github.com>
2022-11-03 15:00:29 -0300

    agregado p0.1 y logica normal para prender bomba

--- TP_Final_ED3.c
+++ TP_Final_ED3.c
@@ -130,11 +130,13 @@ void setPins(){
 
 	//configuramos el p0.0 como salida para riego
 	GPIO_SetDir(0, 0x1, 1);//Configuramos p0.0 como salida
-	LPC_GPIO0->FIOSET |=(1<<0); //Seteo en 1 la salida
-
+	LPC_GPIO0->FIOCLR |=(1<<0); //Seteo en 0 la salida
+	LPC_GPIO0->FIODIR |= (1<<1);//P0.1 como salida
+	LPC_GPIO0->FIOSET |=(1<<1); //Seteo en 1 la salida
+	
 	//configuramos el p0.21 Para activar riego manual
 	LPC_GPIO0->FIODIR |= (1<<21);//P0.21 como salida
-	LPC_GPIO0->FIOSET |= (1<<21);//Seteo en 1 la salida.
+	LPC_GPIO0->FIOCLR |= (1<<21);//Seteo en 0 la salida.
 
 }
 
@@ -388,7 +390,8 @@ void ADC_IRQHandler(){
 	//if((val>3800)){
 	if(humedad<40){
 		if(flag==1){//puedo regar
-		GPIO_ClearValue(0, 0x1);//enciendo la bomba
+		GPIO_SetValue(0, 0x1);//enciendo la bomba
+		LPC_GPIO0->FIOCLR |=(1<<1);
 		}
 	}
 	if(flag==2){//significa que no tengo agua suficiente
@@ -400,7 +403,8 @@ void ADC_IRQHandler(){
 
 void TIMER2_IRQHandler(){
 	if(TIM_GetIntStatus(LPC_TIM2, TIM_MR0_INT)==SET){//pregunto si interrumpio MAT2.0
-		GPIO_SetValue(0, 0x1);//Apago la bomba
+		GPIO_ClearValue(0, 0x1);//Apago la bomba
+		LPC_GPIO0->FIOSET |=(1<<1);
 	}else if(TIM_GetIntStatus(LPC_TIM2, TIM_MR1_INT)==SET){//pregunto si interrumpio MAT2.1
 		offTimer2();//apago TIMER2
 		onAdc();//prendo ADC
@@ -411,7 +415,8 @@ void TIMER2_IRQHandler(){
 
 void EINT3_IRQHandler(){
 	onTimer2();//prendo TIMER2
-	GPIO_ClearValue(0, 0x1);//prendo la bomba
+	GPIO_SetValue(0, 0x1);//prendo la bomba
+	LPC_GPIO0->FIOCLR |=(1<<1);
 	LPC_GPIOINT->IO0IntClr |= (1<<21);//limpio flags de interrupcion
 }
 
